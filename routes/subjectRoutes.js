20a764a0f4c967b9822cc64784a0228406f6381c
VivekTriveda <vivek@trivedatechnologies.com>
2026-08-16 10:47:14 +0530

    Add Swagger documentation for Subject routes
    Added Swagger documentation for the Subject routes endpoint.

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/subjects:
+ *   get:
+ *     summary: Check Subject routes
+ *     description: Test endpoint to verify that Subject routes are working.
+ *     tags:
+ *       - Subject
+ *     responses:
+ *       200:
+ *         description: Subject routes are working successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Subject Routes Working
+ */
+
 // Test Route
 router.get("/", (req, res) => {
     res.json({
